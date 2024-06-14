c332e63170fc61bf933a534697f50170b7ea3758
lauratlee <lauratlee48@gmail.com>
2024-06-14 09:47:45 -0400

    modified sphgen_runner

--- scripts/dock6/sphgen_runner.py
+++ scripts/dock6/sphgen_runner.py
@@ -1,13 +1,9 @@
 
 import os
 
-# os.chdir("pi/summer.thyme-umw/2024_intern_lab_space/thyme_lab_internship_2024/dock6/input_files")
-
 for r,d,f in os.walk(os.getcwd()):
     for dir in d:
-        # testing w/ aa2ar
-        if dir == "aa2ar":
-            os.chdir(dir)
-            os.system("../../../../dock6/bin/sphgen sphgen -i INSPH -o OUTSPH")
-            # os.system("sphgen -i INSPH -o OUTSPH")
+        os.chdir(dir)
+        os.system("../../../../dock6/bin/sphgen sphgen -i INSPH -o OUTSPH")
+        os.chdir("..")
             
