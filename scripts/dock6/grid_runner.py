4cf39abaa058b5482959a1b5efea8090af5d2e0c
lauratlee <lauratlee48@gmail.com>
2024-06-14 15:20:53 -0400

    modified grid_runner

--- scripts/dock6/grid_runner.py
+++ scripts/dock6/grid_runner.py
@@ -3,11 +3,10 @@ import os
 
 for r,d,f in os.walk(os.getcwd()):
     for dir in d:
-        if dir == "aa2ar":
-            os.chdir(dir)
-            os.system("../../../../dock6/bin/grid -i grid.in -o gridinfo.out")
-    
-            os.chdir("..")
+        os.chdir(dir)
+        os.system("../../../../dock6/bin/grid -i grid.in -o gridinfo.out")
+
+        os.chdir("..")
 
 
         
