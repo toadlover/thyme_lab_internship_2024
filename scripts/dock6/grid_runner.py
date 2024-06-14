e1461cfe12ba128e31dda1e4aa90f2ad50aac1be
lauratlee <lauratlee48@gmail.com>
2024-06-14 11:33:46 -0400

    added grid_runner

---
+++ scripts/dock6/grid_runner.py
@@ -0,0 +1,12 @@
+
+import os
+
+for r,d,f in os.walk(os.getcwd()):
+    for dir in d:
+        chdir(dir)
+        os.system("../../../../dock6/bin/grid grid -i grid.in -o gridinfo.out")
+
+        os.chdir("..")
+
+
+        
