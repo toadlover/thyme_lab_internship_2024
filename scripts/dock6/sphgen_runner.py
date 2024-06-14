791367338fdd8f424ab6bbbed6fc9dbfc4016aba
lauratlee <lauratlee48@gmail.com>
2024-06-14 10:36:24 -0400

    modified sphgen_runner

--- scripts/dock6/sphgen_runner.py
+++ scripts/dock6/sphgen_runner.py
@@ -4,7 +4,7 @@ import os
 for r,d,f in os.walk(os.getcwd()):
     for dir in d:
         os.chdir(dir)
-        if ".sph" not in dir:
+        if f"{dir}.sph" not in dir:
             print(f'{dir} is free')
                 
         # os.system("../../../../dock6/bin/sphgen sphgen -i INSPH -o OUTSPH")
