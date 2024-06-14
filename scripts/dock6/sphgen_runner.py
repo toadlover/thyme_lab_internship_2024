62ab4426c3908ab3075504a320081fdd75fad29c
lauratlee <lauratlee48@gmail.com>
2024-06-14 10:44:20 -0400

    added aa2ar_spheres, modified sphgen_runner`

--- scripts/dock6/sphgen_runner.py
+++ scripts/dock6/sphgen_runner.py
@@ -4,8 +4,14 @@ import os
 for r,d,f in os.walk(os.getcwd()):
     for dir in d:
         os.chdir(dir)
-        if f"{dir}.sph" not in dir:
-            print(f'{dir} is free')
+        done = False
+        for r_2,d_2,f_2 in os.walk(os.getcwd()):
+            for file in f_2:
+                if ".sph" in file:
+                    done = True
+            if done = False:
+                print(f"{dir} is free")
+        
                 
         # os.system("../../../../dock6/bin/sphgen sphgen -i INSPH -o OUTSPH")
         os.chdir("..")
