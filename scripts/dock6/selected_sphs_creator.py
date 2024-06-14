606eeb08c10cc476957618ec2014ffa0031bddef
lauratlee <lauratlee48@gmail.com>
2024-06-14 15:42:40 -0400

    modified selected_sphs_creator

--- scripts/dock6/selected_sphs_creator.py
+++ scripts/dock6/selected_sphs_creator.py
@@ -3,8 +3,7 @@ import os
 
 for r,d,f in os.walk(os.getcwd()):
     for dir in d:
-        if dir == "aa2ar":
-            os.chdir(dir)
-            os.system(f"../../../../dock6/bin/sphere_selector {dir}.sph {dir}.mol2 10.0")
+        os.chdir(dir)
+        os.system(f"../../../../dock6/bin/sphere_selector {dir}.sph {dir}.mol2 10.0")
         os.chdir("..")
             
