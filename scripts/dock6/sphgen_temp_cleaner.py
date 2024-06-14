4466e9af24f4cda198a35538ff21297ccf0ca936
lauratlee <lauratlee48@gmail.com>
2024-06-14 11:21:21 -0400

    added temp sphgen cleaner

---
+++ scripts/dock6/sphgen_temp_cleaner.py
@@ -0,0 +1,11 @@
+
+import os
+
+for r,d,f in os.walk(os.getcwd()):
+    for dir in d:
+        os.chdir(dir)
+        for r_2,d_2,f_2 in os.walk(os.getcwd()):
+            for file in f_2:
+                if "temp" in file:
+                    print(file)
+        os.chdir("..")
