27c98c59c90db1bd466ca0e5794655aa2c78666b
lauratlee <lauratlee48@gmail.com>
2024-06-14 10:50:13 -0400

    modified sphgen_runner

--- scripts/dock6/sphgen_runner.py
+++ scripts/dock6/sphgen_runner.py
@@ -4,13 +4,18 @@ import os
 for r,d,f in os.walk(os.getcwd()):
     for dir in d:
         os.chdir(dir)
-        done = False
+        sph_done = False
+        outsph_done = False
         for r_2,d_2,f_2 in os.walk(os.getcwd()):
             for file in f_2:
                 if ".sph" in file:
-                    done = True
-            if done == False:
-                print(f"{dir} is free")
+                    sph_done = True
+                if "OUTSPH" in file:
+                    outsph_done = True
+            if sph_done == True and outsph_done == False:
+                print(f"{dir} needs OUTSPH")
+            if sph_done == False and outsph_done == True:
+                print(f"{dir} needs .sph")
         
                 
         # os.system("../../../../dock6/bin/sphgen sphgen -i INSPH -o OUTSPH")
