3f61f3a15ec1cda77ddcc8adac6d8f701d8950e7
lauratlee <lauratlee48@gmail.com>
2024-06-14 15:54:26 -0400

    modified box_runner

--- scripts/dock6/box_creator.py
+++ scripts/dock6/box_creator.py
@@ -6,5 +6,5 @@ for r,d,f in os.walk(os.getcwd()):
         os.chdir(dir)
         box_path = "showbox.in"
         with open(box_path, 'w') as file:
-            file.write(f"Y \n8.0 \n../002_surface_spheres/selected_spheres.sph \n1 \n{dir}.box.pdb")
+            file.write(f"Y \n8.0 \nselected_spheres.sph \n1 \n{dir}.box.pdb")
         os.chdir("..")
--- scripts/dock6/box_runner.py
+++ scripts/dock6/box_runner.py
@@ -3,7 +3,6 @@ import os
 
 for r,d,f in os.walk(os.getcwd()):
     for dir in d:
-        if dir == "aa2ar":
-            os.chdir(dir)
-            os.system("../../../../dock6/bin/showbox < showbox.in")
+        os.chdir(dir)
+        os.system("../../../../dock6/bin/showbox < showbox.in")
         os.chdir("..")
