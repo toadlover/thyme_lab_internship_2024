158fe2181a0f207319025fd376e66c6b9b9c5e93
lauratlee <lauratlee48@gmail.com>
2024-06-20 13:44:22 -0400

    added nrg_cleaner

---
+++ scripts/dock6/nrg_cleaner.py
@@ -0,0 +1,10 @@
+
+import os
+
+for r,d,f in os.walk(os.getcwd()):
+    for dir in d:
+        if dir == "aa2ar":
+            os.chdir(dir)
+            os.system("rm grid.nrg")
+            os.chdir("..")
+            
