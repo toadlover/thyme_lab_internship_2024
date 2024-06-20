c706591ce040dad3703d94cc47820b4f6a5a53bc
lauratlee <lauratlee48@gmail.com>
2024-06-20 10:49:11 -0400

    modified nrg_compressor

--- scripts/dock6/nrg_compressor.py
+++ scripts/dock6/nrg_compressor.py
@@ -3,12 +3,12 @@ import os
 
 for r,d,f in os.walk(os.getcwd()):
     for dir in d:
-        if dir == "cxcr4":
-            os.chdir(dir)
-            for r_2,d_2,f_2 in os.walk(os.getcwd()):
-                for file in f_2:
-                    if file == "grid.nrg":
-                        os.system("tar -czvf grid_nrg.tar.gz grid.nrg")
-            os.chdir("..")
+        os.chdir(dir)
+        for r_2,d_2,f_2 in os.walk(os.getcwd()):
+            for file in f_2:
+                if file == "grid.nrg":
+                    os.system("tar -czvf grid_nrg.tar.gz grid.nrg")
+                    print(f"{dir} done!")
+        os.chdir("..")
             
 
