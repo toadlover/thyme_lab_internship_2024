beb6fb88a2a137d839c736a30bb790f0c1ceccf2
lauratlee <lauratlee48@gmail.com>
2024-06-20 13:48:02 -0400

    modified nrg_cleaner

--- scripts/dock6/nrg_cleaner.py
+++ scripts/dock6/nrg_cleaner.py
@@ -3,8 +3,9 @@ import os
 
 for r,d,f in os.walk(os.getcwd()):
     for dir in d:
-        if dir == "aa2ar":
+        if dir != "aa2ar":
             os.chdir(dir)
             os.system("rm grid.nrg")
+            print(f"{dir} cleaned")
             os.chdir("..")
             
