fee5359276aa3735abeda7f78dccddd0bd75d806
toadlover <toadloverarig@gmail.com>
2024-07-04 14:49:18 -0400

    Setting up example for running Rosetta. Run instructions with run.sh with an example in its comments

---
+++ rosetta_recovery_of_enamine_papers/rosetta_example/run.sh
@@ -0,0 +1,3 @@
+#/pi/summer.thyme-umw/2024_intern_lab_space/rosetta/source/bin/ligand_discovery_search_protocol.linuxgccrelease @example_rosetta_flags
+#./run.sh /pi/summer.thyme-umw/2024_intern_lab_space/rosetta/source/bin/ligand_discovery_search_protocol.linuxgccrelease example_rosetta_flags
+$1 @$2
