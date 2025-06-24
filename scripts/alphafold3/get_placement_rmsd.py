af7cf9ddbdf62a006216ef475b3e40a0a389e540
Ginsparg <abgvg9@uab.edu>
2025-06-24 14:56:31 -0400

    test cat

--- scripts/alphafold3/get_placement_rmsd.py
+++ scripts/alphafold3/get_placement_rmsd.py
@@ -110,6 +110,8 @@ with pymol2.PyMOL() as pymol:
 						mol = next(pybel.readfile("pdb", r + "/" + dire + "/" + dire + "-lig.pdb"))
 						mol.write("mol2", r + "/" + dire + "/" + dire + "-lig.mol2", overwrite=True)
 
+						os.system("cat " + r + "/" + dire + "/" + dire + "-lig.mol2")
+
 						#reference_ligand = Chem.MolFromPDBFile(r + "/" + dire + "/" + dire + "-lig.pdb", removeHs=True)
 						reference_ligand = Chem.MolFromPDBFile(r + "/" + dire + "/" + dire + "-lig.mol2", removeHs=True)
 						#reference_ligand = Chem.MolFromMol2File(r + "/" + dire + "/crystal_ligand.mol2", removeHs=True)
